f846628f76ff17530356f5a05969c089de0efc27
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2025-11-12 11:38:57 +0100

    demo: explicit effect

--- libs/domain/booking/src/lib/feature-flight/flight-search/flight-search.component.ts
+++ libs/domain/booking/src/lib/feature-flight/flight-search/flight-search.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, computed, effect, signal } from '@angular/core';
+import { Component, computed, effect, signal, untracked } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Flight, FlightFilter, injectTicketsFacade } from '../../logic-flight';
 import { FlightCardComponent, FlightFilterComponent } from '../../ui-flight';
@@ -34,12 +34,13 @@ export class FlightSearchComponent {
 
   constructor() {
     effect(() => console.log(this.route()));
-    effect(() => this.search(this.filter()));
+    effect(() => {
+      this.filter();
+      untracked(() => this.search());
+    });
   }
 
-  protected search(filter: FlightFilter): void {
-    this.filter.set(filter);
-
+  protected search(): void {
     if (!this.filter().from || !this.filter().to) {
       return;
     }
