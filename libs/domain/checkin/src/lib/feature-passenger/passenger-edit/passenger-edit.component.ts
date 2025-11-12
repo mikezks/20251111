6d542f1cca30833475d50399fdb536c3b518d3a9
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2025-11-12 19:43:54 +0100

    demo: advanced signal forms apis

--- libs/domain/checkin/src/lib/feature-passenger/passenger-edit/passenger-edit.component.ts
+++ libs/domain/checkin/src/lib/feature-passenger/passenger-edit/passenger-edit.component.ts
@@ -1,9 +1,34 @@
 import { httpResource } from '@angular/common/http';
-import { Component, effect, input, numberAttribute, signal } from '@angular/core';
+import { Component, input, numberAttribute } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { form, Control } from '@angular/forms/signals';
+import { Control, createProperty, customError, FieldPath, form, property, required, schema, validate } from '@angular/forms/signals';
 import { RouterLink } from '@angular/router';
 import { initialPassenger, Passenger } from '../../logic-passenger';
+
+
+// Custom Field Property
+const ALLOWED_PASSENGER_STATUS = createProperty<string[]>();
+
+// Custom Validator
+function validatePassengerStatus(field: FieldPath<string>, allowedStatus: string[]): void {
+  // Set Custom Field Property
+  property(field, ALLOWED_PASSENGER_STATUS, () => allowedStatus);
+  validate(field, ({ value }) =>
+    !allowedStatus.includes(value())
+      ? customError({
+        kind: 'forbiddenPassengerStatus',
+        message:
+          'This Passenger Status is not allowed. Please use one of the following: ' +
+          allowedStatus.join(', '),
+      }) : undefined
+  );
+}
+
+// (3) Field Logic: required, custom validator, disabled, hidden, readonly, etc.
+const passengerSchema = schema<Passenger>(passengerPath => {
+  required(passengerPath.name);
+  validatePassengerStatus(passengerPath.passengerStatus, ['A', 'C']);
+});
 
 
 @Component({
@@ -11,29 +36,24 @@ import { initialPassenger, Passenger } from '../../logic-passenger';
   imports: [
     ReactiveFormsModule,
     RouterLink,
-    // (3) Field: Template Binding
-    Control, // -> Angular 21: Field
+    // (4) UI Control: Template Binding
+    Control
   ],
   templateUrl: './passenger-edit.component.html'
 })
 export class PassengerEditComponent {
   // (1) Data Model: Writable Signal
-  protected readonly passengerResource = httpResource<Passenger>(() => ({
+  protected passengerResource = httpResource<Passenger>(() => ({
     url: 'https://demo.angulararchitects.io/api/passenger',
     params: { id: this.id() }
   }), { defaultValue: initialPassenger });
 
-  // (2) Field State: valid, touched, dirty, value, disabled, ...
-  protected readonly editForm = form(this.passengerResource.value);
+  // (2) Field State: Meta Data - valid, dirty, touched, etc.
+  protected editForm = form(this.passengerResource.value, passengerSchema);
 
-  readonly id = input(0, { transform: numberAttribute });
-
-  constructor() {
-    effect(() => console.log(this.id()));
-  }
-
+  id = input(0, { transform: numberAttribute });
+  
   protected save(): void {
     console.log(this.editForm().value());
-    console.log(this.passengerResource.value());
   }
 }
