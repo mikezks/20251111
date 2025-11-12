dbf094cebd7653df592356fd396900ffc82bda10
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2025-11-12 19:34:30 +0100

    feat: basic signal store

---
+++ libs/domain/booking/src/lib/logic-flight/state/booking.store.ts
@@ -0,0 +1,45 @@
+import { patchState, signalStore, withComputed, withMethods, withState } from '@ngrx/signals';
+import { rxMethod } from '@ngrx/signals/rxjs-interop';
+import { tapResponse } from '@ngrx/operators';
+import { Flight } from '../model/flight';
+import { FlightFilter } from '../model/flight-filter';
+import { pipe, switchMap } from 'rxjs';
+import { inject } from '@angular/core';
+import { FlightService } from '../data-access/flight.service';
+
+export const BookingStore = signalStore(
+  { providedIn: 'root' },
+  withState({
+    filter: {
+      from: 'Paris',
+      to: 'New York',
+      urgent: false
+    },
+    basket: {
+      3: true,
+      5: true
+    } as Record<number, boolean>,
+    flights: [] as Flight[],
+    selectedFlight: 0
+  }),
+  withComputed(store => ({
+    delayed: () => store.flights().filter(flight => flight.delayed)
+  })),
+  withMethods((
+    store,
+    flightService = inject(FlightService)
+  ) => ({
+    loadFlights: rxMethod<FlightFilter>(pipe(
+      switchMap(filter => flightService.find(
+        filter.from,
+        filter.to,
+        filter.urgent
+      ).pipe(
+        tapResponse({
+          next: flights => patchState(store, { flights }),
+          error: err => console.error(err)
+        })
+      )),
+    ))
+  }))
+);
